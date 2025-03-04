862efd734b33b43a3a641cfa88a14f46fcc5a8d3
Athul-Ramesan <athulrameshankv.ar@gmail.com>
2025-03-04 19:52:26 +0530

    feat: frontend setup done

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,22 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 
+app.use(express.static(path.join(__dirname, '../frontend'), {
+    setHeaders: (res, filePath) => {
+      if (path.extname(filePath) === '.css') {
+        res.setHeader('Content-Type', 'text/css');
+      } else if (path.extname(filePath) === '.js') {
+        res.setHeader('Content-Type', 'application/javascript');
+      }
+    }
+  }));
 
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+  });  
 
 app.listen(port, ()=>{
     console.log("server is runing on PORT :", port )
